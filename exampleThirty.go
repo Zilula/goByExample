a64e7b84668f0759d548b378191e7d60717b2ef8
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-05-21 06:46:52 -0700

    closing channels after a value is recevied

--- exampleThirty.go
+++ exampleThirty.go
@@ -1,8 +1,31 @@
 package main
 
 import (
-	"fmy"
+	"fmt"
 )
 func Thirty() {
-	
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+
+	go func() {
+		for {
+			j, more := <- jobs
+			if more {
+				fmt.Println("Received Job ", j)
+			} else {
+				fmt.Println("Received all jobs") 
+				done <- true
+				return 
+			}
+		}
+	}()
+	for j:= 1; j <= 4; j++ {
+		jobs <- j
+		fmt.Println("Job Sent ", j)
+	}
+	close(jobs)
+	fmt.Println("all jobs Sent")
+
+	<-done
 }
