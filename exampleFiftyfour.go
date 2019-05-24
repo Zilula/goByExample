730cc33cd5f865bbcc443d8324b518c099743b5e
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-05-24 07:45:50 -0700

    SHA1 crypto encoding

---
+++ exampleFiftyfour.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"crypto/sha1"
+	"fmt"
+)
+
+func FiftyFour() {
+	s := "sha1 this string"
+
+	h := sha1.New()
+
+	h.Write([]byte(s))
+
+	bs := h.Sum(nil)
+
+	fmt.Println(s)
+
+	fmt.Printf("%x\n", bs)
+
+	
+}
