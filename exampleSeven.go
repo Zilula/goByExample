218b261407697942e40a73f46a4b9b8cca7621ba
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-05-27 13:09:24 -0700

    Stuff

--- exampleSeven.go
+++ exampleSeven.go
@@ -1,48 +1,48 @@
 package main 
-import "fmt"
-import "time"
+// import "fmt"
+// import "time"
 
-func seven() {
-	i := 2 
-	fmt.Println("write", i, "as")
-	switch i {
-	case 1: 
-		fmt.Println("One")
-	case 2: 
-		fmt.Println("Two")
-	case 3: 
-		fmt.Println("three")
-	}
-
-
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It's the weekend")
-	default: 
-		fmt.Println("its a weekday")
-	}
-
-	t := time.Now()
-	switch {
-	case t.Hour() < 12: 
-		fmt.Println("its before noon")
-	default: 
-		fmt.Println("its after noon")
-	}
+// func seven() {
+// 	i := 2 
+// 	fmt.Println("write", i, "as")
+// 	switch i {
+// 	case 1: 
+// 		fmt.Println("One")
+// 	case 2: 
+// 		fmt.Println("Two")
+// 	case 3: 
+// 		fmt.Println("three")
+// 	}
 
 
+// 	switch time.Now().Weekday() {
+// 	case time.Saturday, time.Sunday:
+// 		fmt.Println("It's the weekend")
+// 	default: 
+// 		fmt.Println("its a weekday")
+// 	}
 
-	whatAmI := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("Ima a boolean")
-		case int : 
-			fmt.Println("Im an int")
-		default: 
-			fmt.Println("Type unknown", t)
-		}	
-	}
-	whatAmI(true)
-	whatAmI(4)
-	whatAmI("hello world")
-}
+// 	t := time.Now()
+// 	switch {
+// 	case t.Hour() < 12: 
+// 		fmt.Println("its before noon")
+// 	default: 
+// 		fmt.Println("its after noon")
+// 	}
+
+
+
+// 	whatAmI := func(i interface{}) {
+// 		switch t := i.(type) {
+// 		case bool:
+// 			fmt.Println("Ima a boolean")
+// 		case int : 
+// 			fmt.Println("Im an int")
+// 		default: 
+// 			fmt.Println("Type unknown", t)
+// 		}	
+// 	}
+// 	whatAmI(true)
+// 	whatAmI(4)
+// 	whatAmI("hello world")
+// }
